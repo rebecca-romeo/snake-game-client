2a65dcc9514a8c0802d3a364755a6dce53a447e5
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-18 13:38:00 -0500

    created module code by adding connect function to client.js. Refactored module exports / imports to include destructuring

--- play.js
+++ play.js
@@ -1,23 +1,2 @@
-const net = require("net");
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: "50541",
-
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // receive data/messge from the server after idling
-  conn.on("data", (data) => {
-    console.log(data)
-  })
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
