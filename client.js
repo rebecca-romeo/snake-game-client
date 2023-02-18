a9d780bd7250c9555d7251a9ed90923be9e1dc9f
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-18 17:15:45 -0500

    refactored coded by creating input module

--- client.js
+++ client.js
@@ -7,46 +7,11 @@ const connect = function() {
     port: "50541",
   });
 
-
   conn.on("connect", () => {
     console.log("success, connected");
     // Displays snake name after connecting
     conn.write("Name: REB");
   });
-
-  // ------ Automatic snake movement on connection
-
-  // Commented out the following because we don't want the snake to automatically move each time it's connected:
-
-  // conn.on("connect", () => {
-
-  //   // Snake goes off screen:
-  //   // setInterval(() => {
-  //   //   conn.write("Move: up");
-  //   // }, 50);
-
-  //   // Snake moves up incrementally:
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 50);
-
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 100);
-
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 150);
-
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 200);
-
-  // });
-
-  // ----- End comment for automatic snake movement
-
-
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -65,3 +30,45 @@ const connect = function() {
 module.exports = {
   connect
 };
+
+
+
+
+
+
+
+
+
+// ------ Automatic snake movement on connection
+
+// previously inside of the connect function
+
+// Commented out the following because we don't want the snake to automatically move each time it's connected:
+
+// conn.on("connect", () => {
+
+//   // Snake goes off screen:
+//   // setInterval(() => {
+//   //   conn.write("Move: up");
+//   // }, 50);
+
+//   // Snake moves up incrementally:
+//   setTimeout(() => {
+//     conn.write("Move: up");
+//   }, 50);
+
+//   setTimeout(() => {
+//     conn.write("Move: up");
+//   }, 100);
+
+//   setTimeout(() => {
+//     conn.write("Move: up");
+//   }, 150);
+
+//   setTimeout(() => {
+//     conn.write("Move: up");
+//   }, 200);
+
+// });
+
+// ----- End comment for automatic snake movement
